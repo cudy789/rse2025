31429e8d156b75235804c09623da6d70be7724e3
Corey <corey.knutson789@gmail.com>
2025-08-06 01:58:21 -0500

    Update candy_drop_machine.ino

--- candy_drop_machine/candy_drop_machine.ino
+++ candy_drop_machine/candy_drop_machine.ino
@@ -1,3 +1,4 @@
+// A library to move the servo
 #include <Servo.h>
 
 // Define pins for motors and sensors
@@ -7,19 +8,17 @@ int servo_pin = 11;
 int red_led_pin = 18;
 int button_pin = 19;
 
+// Spiral disk motor speed (0-255) and the servo angles for the hopper disk (0-180 degrees)
 int disk_speed = 150;
-
 int open_angle = 150;
 int closed_angle = 5;
 
 Servo drop_servo;
 
-
 void setup() {
   pinMode(disk_speed_pin, OUTPUT);
   pinMode(disk_direction_pin, OUTPUT);
   pinMode(red_led_pin, OUTPUT);
-
   pinMode(button_pin, INPUT);
 
   drop_servo.attach(servo_pin);
@@ -34,7 +33,7 @@ void setup() {
 void loop() {
 
   bool button_pressed = digitalRead(button_pin);
-  if (button_pressed){
+  if (button_pressed == true){
     digitalWrite(red_led_pin, HIGH);
     drop_servo.write(open_angle);
     delay(500);
