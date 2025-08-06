643d61e3b901d4afd93e7cfe196133c36d9f3bf9
Corey <corey.knutson789@gmail.com>
2025-08-06 00:09:12 -0500

    Add files via upload

---
+++ candy_drop_machine/candy_drop_machine.ino
@@ -0,0 +1,46 @@
+#include <Servo.h>
+
+// Define pins for motors and sensors
+int disk_speed_pin = 12;
+int disk_direction_pin = 13;
+int servo_pin = 11;
+int red_led_pin = 18;
+int button_pin = 19;
+
+int disk_speed = 150;
+
+int open_angle = 150;
+int closed_angle = 5;
+
+Servo drop_servo;
+
+
+void setup() {
+  pinMode(disk_speed_pin, OUTPUT);
+  pinMode(disk_direction_pin, OUTPUT);
+  pinMode(red_led_pin, OUTPUT);
+
+  pinMode(button_pin, INPUT);
+
+  drop_servo.attach(servo_pin);
+  delay(100); // need a delay sometimes for the servo to connect properly
+  drop_servo.write(closed_angle);
+
+  digitalWrite(disk_direction_pin, HIGH);
+  analogWrite(disk_speed_pin, disk_speed);
+
+}
+
+void loop() {
+
+  bool button_pressed = digitalRead(button_pin);
+  if (button_pressed){
+    digitalWrite(red_led_pin, HIGH);
+    drop_servo.write(open_angle);
+    delay(500);
+    drop_servo.write(closed_angle);
+    delay(500);
+    digitalWrite(red_led_pin, LOW);
+  }
+
+}
